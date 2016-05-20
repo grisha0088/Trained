0f737cf73ae5c33b635aaf11eda1885174cde812
DenisovJr <denisov2gis@gmail.com>
2016-05-20 15:34:40 +0600

    Nlog.config - separate Error file

--- Trained_WPF/Classes/Authorization.cs
+++ Trained_WPF/Classes/Authorization.cs
@@ -9,13 +9,21 @@ namespace Trained_WPF.Classes
         //Проверяем, что пользователь в группе
         public static bool CheckGroups(string domainName, string grouptoCheck)
         {
-            PrincipalContext ctx = new PrincipalContext(ContextType.Domain, domainName); 
-            UserPrincipal user = UserPrincipal.FindByIdentity(ctx, Environment.UserDomainName + "\\" + Environment.UserName); //получаем пользователя            
-
-            if (user != null) 
+            try
             {
-                PrincipalSearchResult<Principal> groups = user.GetGroups(); //полкчаем группы пользователя
-                return groups.Any(p => p.Name == grouptoCheck); //проверяем, есть ли там искомая группа
+                PrincipalContext ctx = new PrincipalContext(ContextType.Domain, domainName);
+                UserPrincipal user = UserPrincipal.FindByIdentity(ctx,
+                    Environment.UserDomainName + "\\" + Environment.UserName); //получаем пользователя            
+
+                if (user != null)
+                {
+                    PrincipalSearchResult<Principal> groups = user.GetGroups(); //полкчаем группы пользователя
+                    return groups.Any(p => p.Name == grouptoCheck); //проверяем, есть ли там искомая группа
+                }
+            }
+            catch (Exception e)
+            {
+                NLog.ExceptionToLog("Error starting app: " + e + "");
             }
             return false;
         }
